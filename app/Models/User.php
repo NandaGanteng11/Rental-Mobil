86940d506af0b0902cd5122c2921fd144e024974
NandaGanteng11 <pradanananda20@gmail.com>
2024-11-21 21:13:01 +0700

    pertemuan ke 7

--- app/Models/User.php
+++ app/Models/User.php
@@ -40,5 +40,10 @@ class User extends Authenticatable
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
+        'password' => 'hashed',
     ];
+
+    public function roles(){
+        return $this->belongsToMany(Role::class);
+    }
 }
